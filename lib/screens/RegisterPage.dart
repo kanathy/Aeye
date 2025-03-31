5dffc4fa35cf8df4f9438300f9f6028091388087
kanathy <shilukamu4@gmail.com>
2025-03-31 10:50:11 +0530

    add file

---
+++ lib/screens/RegisterPage.dart
@@ -0,0 +1,10 @@
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Your UI
+    );
+  }
+}
