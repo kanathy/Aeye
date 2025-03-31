891ca770111f831c4b61dbddbcf988934920d99f
kanathy <shilukamu4@gmail.com>
2025-03-31 22:24:23 +0530

    add file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/RegisterPage.dart';
+import 'screens/RegisterPage.dart'; // Import your screen here
+import 'screens/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RegisterPage(),
+      home: RegisterPage(), // This is your custom screen
     );
   }
 }
